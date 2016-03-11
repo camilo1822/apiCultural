7a8916b652d2cebd0efb853c9eec931d7af1ac9c
camilo-1822@hotmail.com <camilo1822@gmail.com>
2016-03-10 21:20:22 -0500

    integracion heroku

--- server.js
+++ server.js
@@ -43,12 +43,13 @@ listaLugares.route('/listaLugares/:id')
 
 app.use(listaLugares);
 
-
+//mongodb://lugares:apicultural@ds011379.mlab.com:11379/heroku_2v8qghk7
+//mongoose.connect('mongodb://localhost/listaLugares', function(err, res) {
 mongoose.connect('mongodb://lugares:apicultural@ds011379.mlab.com:11379/heroku_2v8qghk7', function(err, res) {  
   if(err) {
     console.log('ERROR: connecting to Database. ' + err);
   }
-  app.listen(5000, function() {
+  app.listen(27017, function() {
     console.log("Node server running on http://localhost:3000");
   });
 });
