698077141c1ca6c8157e5b9995ed4ced9f33d465
camilo-1822@hotmail.com <camilo1822@gmail.com>
2016-03-15 18:52:45 -0500

    herokuu

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ var models     = require('./models/lugares')(app, mongoose);
 var LugaresCtrl = require('./controllers/listaLugares');
 
 
-var uri = 'mongodb://10.239.188.122:42158@ds011379.mlab.com:11379/heroku_2v8qghk7';
-db = mongoose.createConnection(uri);
+/*var uri = 'mongodb://10.239.188.122:42158@ds011379.mlab.com:11379/heroku_2v8qghk7';
+db = mongoose.createConnection(uri);*/
 
 
 // API routes
@@ -31,6 +31,29 @@ listaLugares.route('/listaLugares/:id')
 app.use(listaLugares);
 
 
+
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/listaLugares';
+
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var port = process.env.PORT || 5000;
+
+app.use('/api/v1', router);
+
+app.listen(port, function() {
+  console.log('Node Server Running in the port:'+port);
+});
 
 
 
@@ -47,7 +70,7 @@ app.use(listaLugares);
   console.log("after save");*/
 
 
-app.set('port', (process.env.PORT || 5000))
+/*app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
@@ -56,4 +79,4 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+})*/
