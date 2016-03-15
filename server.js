b2b471cccaad95f753fb8dfaae540ef2a329fc3d
camilo-1822@hotmail.com <camilo1822@gmail.com>
2016-03-14 21:50:10 -0500

    herokuu

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ var db;
 
 //mongodb://lugares:apicultural@ds011379.mlab.com:11379/heroku_2v8qghk7
 //mongoose.connect('mongodb://localhost/listaLugares', function(err, res) {
-mongoose.connect('mongodb://lugares:apicultural@ds011379.mlab.com:11379/heroku_2v8qghk7', function(err, res) {  
+mongoose.connect('mongodb://heroku_2v8qghk7:ds011379.mlab.com:11379/heroku_2v8qghk7', function(err, res) {  
   if(err) {
     console.log('ERROR: connecting to Database. ' + err);
   }
