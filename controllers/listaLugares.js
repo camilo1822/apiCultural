a02807dd6a1d6699ab636bfd713b33493d6a14c0
camilo-1822@hotmail.com <camilo1822@gmail.com>
2016-03-16 22:51:35 -0500

    hola

--- controllers/listaLugares.js
+++ controllers/listaLugares.js
@@ -6,20 +6,7 @@ var Lugares  = mongoose.model('lugares');
 exports.findAllLugares = function(req, res) {
 	Lugares.find(function(err, listaLugares) {
     if(err) res.send(500, err.message);
-    if(listaLugares === null){
-      console.log('Customer not found');
-      res.send('no hay datos');
-      res.status(404).jsonp({error: 'Customer not found'});
-    }
 
-
-    if(listaLugares != null){
-      console.log('Customer not found');
-      res.send('si hay datos');
-      res.status(404).jsonp({error: 'Customer not found'});
-    }
-
-    res.send('estoy entrando');
     console.log('GET api/listaDeLugares')
 		res.status(200).jsonp(listaLugares);
 	});
