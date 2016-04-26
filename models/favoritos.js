587fef9851479dd9195f4119b8d6a929d7bb272d
camilo-1822@hotmail.com <camilo1822@gmail.com>
2016-04-26 15:06:13 -0500

    agregar favorito

--- models/favoritos.js
+++ models/favoritos.js
@@ -3,11 +3,7 @@ exports = module.exports = function(app, mongoose) {
   var favoritosSchema = new mongoose.Schema({
     id_user:    { type: String },
     title:    { type: String },
-    sites:    [
-       {type: String}
-    
-    ]
-   
+    sites:  {type: String}
   });
 
   mongoose.model('favoritos', favoritosSchema);
